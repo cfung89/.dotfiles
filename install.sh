045b6cf1506d3675b6f324fc35802ea13724bb5f
Cyrus <cyhfung@gmail.com>
2025-01-21 23:44:18 -0500

    installation script

---
+++ install.sh
@@ -0,0 +1,50 @@
+#! /bin/bash
+
+set -o pipefail
+
+cd
+
+read -p "There are 2 package managers that can be used for installation.
+Type the corresponding number:
+    1) APT      2) pacman
+
+Enter a selection (default=1): " distro
+
+git clone https://github.com/cfung89/.dotfiles.git $HOME/temp/.config
+
+if [[ $distro == 1 || $distro == "" ]] ; then
+    echo "Installing packages with APT"
+    apt update && apt upgrade
+    apt install -y --no-install-recommends \
+        git \
+        fzf \
+        tmux
+
+    echo "Installing neovim from source"
+    apt-get install ninja-build gettext cmake curl build-essential
+    git clone https://github.com/neovim/neovim
+    cd neovim
+    make CMAKE_BUILD_TYPE=RelWithDebInfo
+    cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb || make install   # if this doesn't work, try `sudo make install`
+
+elif [[ $distro -eq 2 ]]; then
+    echo "Installing packages with pacman"
+    pacman -S --noconfirm \
+        git \
+        fzf \
+        tmux \
+        neovim \
+        python3
+else
+    echo "Error: Invalid input"
+fi
+
+echo "Installing tmux plugins"
+cd $HOME/temp/.config/tmux
+mkdir plugins
+git clone https://github.com/tmux-plugins/tpm $HOME/temp/.config/tmux/plugins/tpm
+git clone -b v2.1.2 https://github.com/catppuccin/tmux.git $HOME/temp/.config/tmux/plugins/tmux
+echo "You can now reload tmux plugins with Prefix + Shift + I
+"
+
+echo "Installation complete"
