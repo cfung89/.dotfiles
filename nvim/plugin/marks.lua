a571479c9bd9d9653844ee5ca0305c7b3cc7aea4
Cyrus <cyhfung@gmail.com>
2025-05-06 14:31:38 -0400

    marks

--- nvim/plugin/marks.lua
+++ nvim/plugin/marks.lua
@@ -1,13 +1,21 @@
 vim.keymap.set("n", "m", function()
-	local char = string.char(vim.fn.getchar())
+	local ok, getChar = pcall(vim.fn.getchar)
+	if not ok then
+		return
+	end
+	local char = string.char(getChar)
 	local buf = vim.api.nvim_get_current_buf()
-	local lnum = vim.fn.line('.')
+	local lnum = vim.fn.line(".")
 	vim.api.nvim_buf_set_mark(buf, char, lnum, 1, {})
 	vim.print("Created mark '" .. char .. "'")
 end, { desc = "Create mark." })
 
 vim.keymap.set("n", "dm", function()
-	local char = string.char(vim.fn.getchar())
+	local ok, getChar = pcall(vim.fn.getchar)
+	if not ok then
+		return
+	end
+	local char = string.char(getChar)
 	local buf = vim.api.nvim_get_current_buf()
 	vim.api.nvim_buf_del_mark(buf, char)
 	vim.print("Deleted mark '" .. char .. "'")
